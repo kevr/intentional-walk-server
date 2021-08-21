2df752530b0b1f7783203b9b85be9042d392b720
Ed Lee <leefiftyone@gmail.com>
2021-08-21 00:33:35 +0000

    Allow data import from csv

---
+++ home/management/commands/dataimport.py
@@ -0,0 +1,51 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from home.models import Device, DailyWalk, IntentionalWalk
+from home.utils.generators import AccountGenerator, DeviceGenerator
+
+class Command(BaseCommand):
+    help = "Import data"
+
+    def add_arguments(self, parser):
+        parser.add_argument("FILENAME")
+        parser.add_argument("-t", "--type", required=True, choices=["daily", "intentional"])
+
+    def handle(self, *args, **options):
+        if options["FILENAME"].endswith(".csv"):
+            self._import_csv(*args, **options)
+
+    def _import_csv(*args, **options):
+        acct_gen = AccountGenerator()
+        dev_gen = DeviceGenerator()
+
+        with open(options["FILENAME"], "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                device_id = row["device_id"]
+                try:
+                    device = Device.objects.get(device_id=device_id)
+                except Device.DoesNotExist:
+                    account = next(acct_gen.generate(1))
+                    device = next(dev_gen.generate(1, account=account, device_id=device_id))
+
+                if options["type"] == "daily":
+                    DailyWalk.objects.get_or_create(
+                        date=row["date"],
+                        steps=row["steps"],
+                        distance=row["distance"],
+                        device=device,
+                    )
+                elif options["type"] == "intentional":
+                    IntentionalWalk.objects.get_or_create(
+                        event_id=row["event_id"],
+                        start=row["start"],
+                        end=row["end"],
+                        steps=row["steps"],
+                        pause_time=row["pause_time"],
+                        distance=row["distance"],
+                        device=device,
+                    )
+                else:
+                    raise ValueError(f"Don't know what to do with {options['type']}")
